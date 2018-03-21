4ab9c00b174e8096f7a4cdb6303be721cb75e107
Chris <christopher.cooper15@gmail.com>
2018-03-21 14:33:22 -0500

    Moving and Shooting
    Space to jump, left control/left mouse to fire

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+	private PlayerMovement move;
+	private Animator anim;
+	[SerializeField]
+	private Rigidbody2D arrow;
+	[SerializeField]
+	private Transform firePoint;
+	public float speed = 20f;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		move = GetComponent<PlayerMovement> ();
+	}
+	
+	void Update () {
+		FireArrow ();
+	}
+
+	void FireArrow(){
+		if (Input.GetButtonDown ("Fire1")) {
+			anim.SetTrigger ("FireArrow");
+
+
+		}
+	}
+	public void ArrowOne(){
+		if (move.facingRight) {
+			Rigidbody2D arrowIns = Instantiate (arrow, firePoint.position, Quaternion.Euler (new Vector3 (0, 0, -90))) as Rigidbody2D;
+			arrowIns.velocity = new Vector2 (speed, 0);
+		} else {
+			Rigidbody2D arrowIns = Instantiate (arrow, firePoint.position, Quaternion.Euler (new Vector3 (0, 0, 90))) as Rigidbody2D;
+			arrowIns.velocity = new Vector2 (-speed, 0);
+		}
+	}
+
+}
